92a9115767e3e5df1468c497b085566080d8dab2
Mateusz Szczepański <mateusz.szczepanski.kontakt@gmail.com>
2026-03-24 16:37:50 +0100

    Revert "test commit with syntax error"
    This reverts commit c2915054877e965147248717e9d7aa51e153d888.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,3 +1,3 @@
 // This project does not use Astro Content Collections.
 // This empty config silences the "Content config not loaded" dev-server warning.
-export const collections = {}};
+export const collections = {};
